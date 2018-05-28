371cd3cd90f5d1671641218615f9dd767148452a
Maciej Miszczyk <maciejmmiszczyk@gmail.com>
2018-05-28 19:38:31 +0200

    Main loop redesign

--- deepmage.py
+++ deepmage.py
@@ -10,6 +10,136 @@ BIT_MODE = 1
 HEX_MODE = 4
 
 
+class UI(object):
+    def __init__(self, screen, file):
+        self.screen = screen
+        self.file = file
+        self.reader = bitstream_reader.FileReader(self.file, 1024)
+        reader.set_wordsize(8)
+        self.mode = HEX_MODE
+        self.representation = hex_representation
+        self.cursor = (0, 0)
+        self.old_cursor = self.cursor
+        self.cursor_at_word = 0
+        self.starting_word = 0
+        self.chars_per_word = math.ceil(reader.get_wordsize() / self.mode)
+        self.words_in_line = self.calculate_words_in_line()
+        self.lines = self.screen.height - 2  # 1 line for header and 1 for footer
+        self.total_words = reader.get_wordcount()
+        self.words_in_view = self.lines * self.words_in_line
+        self.view = reader.get_view(self.starting_word, self.words_in_view)
+        self.view_changed = True
+        self.screen.print_at(self.make_header_text(),
+                             0, 0,
+                             Screen.COLOUR_CYAN,
+                             Screen.A_BOLD and Screen.A_REVERSE)
+        self.main_loop_internal()
+
+    def draw_view(self):
+        self.view = reader.get_view(self.starting_word, self.words_in_view)
+        for line_number in range(self.lines):
+            pos = 0
+            for word_number in range(self.words_in_line):
+                current_word = line_number * self.words_in_line + word_number
+                if current_word + self.starting_word >= self.total_words:
+                    self.screen.print_at((' ' * self.mode), pos, line_number + 1)
+                else:
+                    self.screen.print_at(self.representation(self.view[current_word]),
+                                         pos, line_number + 1)
+                pos += self.chars_per_word
+                self.screen.print_at(' ', pos, line_number + 1)
+                pos += 1
+        self.view_changed = False
+        self.old_cursor = self.cursor
+
+    def calculate_words_in_line(self):
+        return max(1, self.screen.width //
+                   (self.chars_per_word + 1))  # 1 char for a separator
+
+    def make_header_text(self):
+        if len(self.file.name) == self.screen.width:
+            return self.file.name
+        if len(self.file.name) > self.screen.width:
+            return self.file.name[:self.screen.width - 3] + '...'
+        pad = ' ' * ((self.screen.width - len(self.file.name)) // 2)
+        return pad + self.file.name + pad
+
+    def handle_cursor_move(self):
+        self.cursor_at_word = self.starting_word + (self.cursor[1] * self.words_in_line) + self.cursor[0]
+        self.screen.print_at(('{}/{}' + ' ' * self.screen.width).format(
+            self.cursor_at_word + 1, self.total_words), 0, self.screen.height - 1)
+        self.screen.print_at(self.representation(self.view[self.old_cursor[1] * self.words_in_line +
+                                                           self.old_cursor[0]]),
+                             self.old_cursor[0] * (self.chars_per_word + 1), self.old_cursor[1] + 1,
+                             attr=Screen.A_NORMAL)
+        self.screen.print_at(self.representation(self.view[self.cursor[1] * self.words_in_line + self.cursor[0]]),
+                             self.cursor[0] * (self.chars_per_word + 1), self.cursor[1] + 1,
+                             attr=Screen.A_REVERSE)
+        self.old_cursor = None
+
+    def handle_screen_resize(self):
+        self.chars_per_word = math.ceil(reader.get_wordsize() / self.mode)
+        self.words_in_line = self.calculate_words_in_line()
+        self.lines = self.screen.height - 2  # 1 line for header and 1 for footer
+        self.words_in_view = self.lines * self.words_in_line
+        self.screen = Screen.wrapper(main_loop)
+        self.screen.clear()
+
+    def redraw_if_needed(self):
+        needs_refresh = False
+        if self.screen.has_resized():
+            self.handle_screen_resize()
+            needs_refresh = True
+        if self.view_changed:
+            self.draw_view()
+            needs_refresh = True
+        if self.old_cursor is not None:
+            try:
+                self.handle_cursor_move()
+            except IndexError:
+                pass
+            needs_refresh = True
+        if needs_refresh:
+            self.screen.refresh()
+
+    def handle_keyboard_events(self, k):
+        old_start = self.starting_word
+        if (k == Screen.KEY_RIGHT) and self.cursor_at_word < self.total_words - 1:
+            self.old_cursor = self.cursor
+            self.cursor = self.cursor[0] + 1, self.cursor[1]
+        elif (k == Screen.KEY_LEFT) and self.cursor_at_word > 0:
+            self.old_cursor = self.cursor
+            self.cursor = self.cursor[0] - 1, self.cursor[1]
+        elif (k == Screen.KEY_DOWN) and self.cursor_at_word < self.total_words - self.words_in_line:
+            self.old_cursor = self.cursor
+            self.cursor = self.cursor[0], self.cursor[1] + 1
+        elif (k == Screen.KEY_UP) and self.cursor_at_word > self.words_in_line - 1:
+            self.old_cursor = self.cursor
+            self.cursor = self.cursor[0], self.cursor[1] - 1
+        if self.cursor[0] >= self.words_in_line:
+            self.cursor = 0, self.cursor[1] + 1
+        if self.cursor[0] < 0:
+            self.cursor = self.words_in_line - 1, self.cursor[1] - 1
+        if self.cursor[1] < 0:
+            self.starting_word = self.starting_word - self.words_in_line if self.starting_word > 0 else 0
+            self.cursor = self.cursor[0], 0
+        if self.cursor[1] >= self.lines:
+            self.starting_word = self.starting_word + self.words_in_line
+            self.cursor = self.cursor[0], self.lines - 1
+        self.starting_word = min(self.starting_word, self.total_words - self.words_in_view +
+                                 (self.starting_word % self.words_in_view))
+        self.starting_word = max(self.starting_word, 0)
+        if old_start != self.starting_word:
+            self.view_changed = True
+
+    def main_loop_internal(self):
+        while True:
+            self.redraw_if_needed()
+            e = self.screen.get_event()
+            if type(e) == KeyboardEvent:
+                self.handle_keyboard_events(e.key_code)
+
+
 def hex_representation(word):
     if word is None or len(word) == 0:
         return []
@@ -27,97 +157,15 @@ def bit_representation(word):
     return bitstring.BitString(word).bin
 
 
-def calculate_words_in_line(chars_per_word, screen_width):
-    return max(1, screen_width //
-               (chars_per_word + 1))  # 1 char for a separator
-
-
-def make_header_text(text, width):
-    if len(text) == width:
-        return text
-    if len(text) > width:
-        return text[:width - 3] + '...'
-    pad = ' ' * ((width - len(text)) // 2)
-    return pad + text + pad
-
-
 # TODO: open file provided by user
 with open('a.bin', 'r+b') as f:
     reader = bitstream_reader.FileReader(f, 1024)
 
     # TODO: modifying file contents
-    # TODO: redesign loop for performance (don't redraw everything if we don't have to)
     # TODO: keyboard shortcuts for switching hex/bin mode, changing word size, saving, quitting
     # TODO: catch KeyboardInterrupt
     def main_loop(screen):
-        mode = HEX_MODE
-        cursor = (0, 0)
-        starting_word = 0
-        reader.set_wordsize(8)
-        while True:
-            chars_per_word = math.ceil(reader.get_wordsize() / mode)
-            words_in_line = calculate_words_in_line(chars_per_word,
-                                                    screen.width)
-            lines = screen.height - 2  # 1 line for header and 1 for footer
-            total_words = reader.get_wordcount()
-            words_in_view = lines * words_in_line
-            starting_word = min(starting_word, total_words - words_in_view + (starting_word % words_in_view))
-            starting_word = max(starting_word, 0)
-            view = reader.get_view(starting_word, words_in_view)
-            representation = bit_representation if mode == BIT_MODE else hex_representation
-
-            screen.print_at(make_header_text(f.name, screen.width),
-                            0, 0,
-                            Screen.COLOUR_CYAN,
-                            Screen.A_BOLD and Screen.A_REVERSE)
-            cursor_at_word = starting_word + (cursor[1] * words_in_line) + cursor[0]
-            screen.print_at(('{}/{}' + ' ' * screen.width).format(
-                cursor_at_word + 1, total_words), 0, screen.height - 1)
-
-            for line_number in range(lines):
-                pos = 0
-                for word_number in range(words_in_line):
-                    current_word = line_number * words_in_line + word_number
-                    if current_word + starting_word >= total_words:
-                        screen.print_at((' ' * mode), pos, line_number + 1)
-                        pass
-                    else:
-                        screen.print_at(representation(view[current_word]),
-                                        pos, line_number + 1,
-                                        attr=Screen.A_REVERSE if (word_number == cursor[0] and line_number == cursor[1])
-                                        else Screen.A_NORMAL)
-                    pos += chars_per_word
-                    screen.print_at(' ', pos, line_number + 1)
-                    pos += 1
-
-            while True:
-                if screen.has_resized():
-                    screen = Screen.wrapper(main_loop)
-                    screen.clear()
-                screen.refresh()
-                e = screen.get_event()
-                if e is None or type(e) != KeyboardEvent:
-                    continue
-                k = e.key_code
-                if (k == Screen.KEY_RIGHT) and cursor_at_word < total_words - 1:
-                    cursor = cursor[0] + 1, cursor[1]
-                elif (k == Screen.KEY_LEFT) and cursor_at_word > 0:
-                    cursor = cursor[0] - 1, cursor[1]
-                elif (k == Screen.KEY_DOWN) and cursor_at_word < total_words - words_in_line:
-                    cursor = cursor[0], cursor[1] + 1
-                elif (k == Screen.KEY_UP) and cursor_at_word > words_in_line - 1:
-                    cursor = cursor[0], cursor[1] - 1
-                if cursor[0] >= words_in_line:
-                    cursor = 0, cursor[1] + 1
-                if cursor[0] < 0:
-                    cursor = words_in_line - 1, cursor[1] - 1
-                if cursor[1] < 0:
-                    starting_word = starting_word - words_in_line if starting_word > 0 else 0
-                    cursor = cursor[0], 0
-                if cursor[1] >= lines:
-                    starting_word = starting_word + words_in_line
-                    cursor = cursor[0], lines - 1
-                break
+        UI(screen, f)
 
 
     Screen.wrapper(main_loop)
