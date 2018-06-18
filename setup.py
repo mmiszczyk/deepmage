8351a5f43744839dd8ac63be5467c49de08799aa
Maciej Miszczyk <maciejmmiszczyk@gmail.com>
2018-06-18 20:10:23 +0200

    Setup scripts

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='deepmage',
+    version='0.1',
+    packages=['libdeepmage'],
+    url='https://github.com/mmiszczyk/deepmage',
+    license='GPL 3',
+    author='Maciej Miszczyk',
+    author_email='maciejmmiszczyk@gmail.com',
+    description='Hex editor for bit-level occultism',
+    install_requires=['hy', 'bitstring', 'asciimatics'],
+    python_requires='>=3',
+    package_data={'': '*.hy'},
+    scripts=('deepmage',),
+    classifiers=('Programming Language :: Python :: 3',
+                 'Programming Language :: Lisp',
+                 'Operating System :: OS Independent',
+                 'Environment :: Console',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Information Technology',
+                 'Intended Audience :: System Administrators',
+                 'Intended Audience :: Telecommunications Industry',
+                 'Topic :: Security',
+                 'Topic :: Software Development :: Embedded Systems')
+)
